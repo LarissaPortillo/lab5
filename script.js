4f53e19e2666f49eccf46f62f9a2c7d4aec0de08
Glitch (hello-webpage) <none>
2020-10-16 07:28:55 +0000

    👍🎊 Checkpoint ./script.js:21940894/633 ./.glitch-assets:21940894/390 ./index.html:21940894/1940

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+d3.csv("https://cdn.glitch.com/53c237f9-1754-427d-899a-6c561b761a18%2Fcoffee-house-chains.csv?v=1602833142227", d3.autoType)
+   .then(data=>{
+  data=data;
+  console.log(data);
+  
+})
