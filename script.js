60021f60e833977cf528ae1a668c95fdc1240a67
Glitch (hello-webpage) <none>
2020-10-18 22:22:50 +0000

    👓📟 Checkpoint ./index.html:21940894/5 ./script.js:21940894/385 ./.glitch-assets:21940894/1054

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ d3.csv("https://cdn.glitch.com/53c237f9-1754-427d-899a-6c561b761a18%2Fcoffee-hou
   data=data;
   console.log(data);
   
+  const margin = {top: 20, right: 10, bottom: 20, left: 10};
+  const width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+  
+ 
+  
+  
 })
