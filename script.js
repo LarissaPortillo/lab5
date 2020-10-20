6fdb861d3ee86cbbab15cc5a494ae4dc9113c126
Glitch (hello-webpage) <none>
2020-10-20 14:00:58 +0000

    👳🏠 Checkpoint ./script.js:21940894/423

--- script.js
+++ script.js
@@ -16,14 +16,22 @@ d3.csv("https://cdn.glitch.com/53c237f9-1754-427d-899a-6c561b761a18%2Fcoffee-hou
  const company=data.map(d=>d.company);
  console.log(company);
   
+ const maxRev = d3.max(data, (d)=>{return d.revenue;});
+ console.log('maxRev',maxRev);
+  
+ const maxStore = d3.max(data, (d)=>{return d.stores;});
+ console.log('maxStore',maxStore);
+  
  const xScale = d3.scaleBand()
   //.domain(d3.range(company.length))
   .domain(company)
   .rangeRound([0, width])
   .paddingInner(0.1);
 
+  
+//add an if for maxRev or maxStore
 const yScale = d3.scaleLinear()
-    .domain(d3.extent(data, (d)=>{return d.LifeExpectancy;}))
+    .domain([0,maxRev])
     .range([height,0])
     .clamp(true);
           
